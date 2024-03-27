9987c733b47936705dfcc0a51070591b23e8615a
ZokirovZubayr <zokirovzubayr@gmail.com>
2024-03-26 23:39:07 -0700

    I want to learn so I add this commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function max(a,b) {
+    let c;
+    c = Math.max(a,b);
+    alert("Kiritilgan 2 ta soning kattasi = " + c);
+
+}
+let a = Number(prompt("1 soni kriting..."));
+let b = Number(prompt("2 soni kriting..."));
+max(a,b);
